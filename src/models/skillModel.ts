c687075a939ef6bf000eccbdaa12098df1527ed9
Daniel <daniel.seebacher17@gmail.com>
2025-08-29 15:55:59 +0200

    Update to skill model

--- src/models/skillModel.ts
+++ src/models/skillModel.ts
@@ -1,11 +1,5 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-} from 'typeorm';
-
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
+import { Project } from './projectModel';
 @Entity()
 export class Skill {
   @PrimaryGeneratedColumn('uuid')
@@ -16,4 +10,12 @@ export class Skill {
 
   @Column({ type: 'text', length: 200 })
   description: string;
+
+  @ManyToMany(() => Project, project => project.skills, { eager: true })
+  @JoinTable({
+    name: 'skill_projects', // Junction table name
+    joinColumn: { name: 'skill_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'project_id', referencedColumnName: 'id' },
+  })
+  projects: Project[];
 }
