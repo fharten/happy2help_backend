85133b13cc89c0b2d1b02305651aa58ffffbbefa
Dirk <so0mewhat2@gmail.com>
2025-09-02 11:25:04 +0200

    refactor: use plural routes for users, ngos and projects in applicationRoutes

--- src/routes/applicationRoutes.ts
+++ src/routes/applicationRoutes.ts
@@ -4,14 +4,14 @@ import { ApplicationController } from '../controllers/applicationController';
 const router = Router();
 const applicationController = new ApplicationController();
 
-// GET ALL APPLICATIONS BY USER | /applications/user/:userId
-router.get('/user/:userId', applicationController.getAllApplicationsByUserId);
+// GET ALL APPLICATIONS BY USER | /applications/users/:userId
+router.get('/users/:userId', applicationController.getAllApplicationsByUserId);
 
-// GET ALL APPLICATIONS BY NGO | /applications/ngo/:ngoId
-router.get('/ngo/:ngoId', applicationController.getAllApplicationsByNgoId);
+// GET ALL APPLICATIONS BY NGO | /applications/ngos/:ngoId
+router.get('/ngos/:ngoId', applicationController.getAllApplicationsByNgoId);
 
-// GET ALL APPLICATIONS BY PROJECT | /applications/project/:projectId
-router.get('/project/:projectId', applicationController.getAllApplicationsByProjectId);
+// GET ALL APPLICATIONS BY PROJECT | /applications/projects/:projectId
+router.get('/projects/:projectId', applicationController.getAllApplicationsByProjectId);
 
 // CREATE APPLICATION | /applications
 router.post('/', applicationController.createApplicationByProjectId);
