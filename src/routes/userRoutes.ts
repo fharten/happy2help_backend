8973af1294103bc3a50e200bf5d6d32e0cc23e41
Daniel <daniel.seebacher17@gmail.com>
2025-09-02 10:56:02 +0200

    Fixed typos in comments and route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,19 +7,19 @@ const userController = new UserController();
 // GET ALL USERss | /users/activated
 router.get('/', userController.getAllUsers);
 
-// // GET all ACTIVATED USERs | /users/activated
-router.get('/activated', userController.getAllActiveUsers);
+// GET all ACTIVATE USERs | /users/active
+router.get('/active', userController.getAllActiveUsers);
 
-// // CREATE USER | /users
+// CREATE USER | /users
 router.post('/', userController.createUser);
 
-// // GET SINGLE USER | /users/:id
+// GET SINGLE USER | /users/:id
 router.get('/:id', userController.getUserById);
 
-// // UPDATE SINGLE USER | /users/:id
+// UPDATE SINGLE USER | /users/:id
 router.put('/:id', userController.updateUserById);
 
-// // DELETE SINGLE USER | /users/:id
+// DELETE SINGLE USER | /users/:id
 router.delete('/:id', userController.deleteUserById);
 
 export default router;
